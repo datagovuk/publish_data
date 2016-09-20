bbb4898429f82d562b25776d996c1ee2b471b34d
Harry <harryroberttrimble@gmail.com>
2016-09-20 11:33:56 +0100

    second commit

---
+++ app/routes.js
@@ -0,0 +1,55 @@
+  var express = require('express');
+var router = express.Router();
+
+router.use(function (req, res, next) {
+
+ // Store common vars
+
+ res.locals.formData = "";
+ res.locals.formQuery = "?";
+ res.locals.data = {};
+
+ for (var name in req.query){
+   var value = req.query[name];
+   res.locals.formData += '<input type="hidden" name="'+name+'" value="' + value + '">\n';
+   res.locals.formQuery += name + "=" + value + "&";
+   res.locals.data[name] = value;
+ }
+
+ res.locals.formQuery = res.locals.formQuery.slice(0,-1);
+
+ next();
+ 
+});
+
+router.get('/', function (req, res) {
+  res.render('index');
+
+});
+
+
+// Branching
+
+// Question for questions/eligibility/index.html
+router.get('/create_account/enter_details', function (req, res) {
+
+  console.log("existing_account");
+
+  // get the answer from the query string (eg. ?existing_account="yes")
+  var existing_account = req.query.existing_account;
+
+  if (existing_account == "yes" ){
+
+    // if users is DOES have account
+    res.redirect("/create_account/has_existing_account" + res.locals.formQuery);
+
+  } else {
+
+    // if users is does NOT have account
+    res.render('create_account/enter_details.html');
+
+  }
+
+});
+
+module.exports = router;
