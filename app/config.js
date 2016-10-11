763e3412b4667a4c718600a86ff74c82348de8a1
maria-izquierdo <maria.izquierdo@digital.cabinet-office.gov.uk>
2016-10-11 15:45:12 +0100

    change name of the service
    change 'Publish government data' to 'Publish data'

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "Publish government data",
+  serviceName: "Publish data",
 
   // Default port that prototype runs on
   port: '3000',
