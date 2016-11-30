a5c2f25ab4b0fcc71b55728869df910f675ed54e
Max Froumentin <max@froumentin.net>
2016-11-30 16:27:49 +0000

    Copy changes and flow simplifications - remove themes - replace file with links - remove unused templates - rename country to region

--- app/session.js
+++ app/session.js
@@ -5,7 +5,7 @@ module.exports = {
       {
         title: 'Price Paid Data',
         summary: 'Price Paid Data tracks the residential property sales in England and Wales that are lodged with Land Registry for registration.',
-        files: [{
+        links: [{
           url: 'http://prod.publicdata.landregistry.gov.uk.s3-website-eu-west-1.amazonaws.com/pp-monthly-update-new-version.csv',
           title: 'November 2016'
         },{
@@ -15,9 +15,7 @@ module.exports = {
         ],
         licence: 'Open Government Licence',
         otherLicence: '',
-        themes: [ 'Crime and justice', 'Environment' ],
-        geo: [ 'England' ],
-        otherGeo: '',
+        geo: [ 'england' ],
         periodMonth: '1',
         periodYear: '2016',
         frequency: 'month',
@@ -27,7 +25,7 @@ module.exports = {
       {
         title: 'INSPIRE View Service and Metadata',
         summary: 'Land Registry INSPIRE View Service is a Web Mapping Service (WMS) which provides map images of Land Registry\'s INSPIRE index polygons',
-        files: [{
+        links: [{
           url: 'http://inspire.landregistry.gov.uk/inspire/ows?Service=WMS&Request=Getcapabilities',
           title: 'November 2016'
         },{
@@ -35,12 +33,10 @@ module.exports = {
           title: 'December 2016'
         }
         ],
-        files: [],
+        links: [],
         licence: 'Open Government Licence',
         otherLicence: '',
-        themes: [ 'Crime and justice', 'Environment' ],
-        geo: [ 'England' ],
-        otherGeo: '',
+        geo: [ 'england' ],
         startDay: '1',
         startMonth: '1',
         startYear: '2016',
